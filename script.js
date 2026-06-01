d1ffea858e8788894fb37b04b8d80ddafeec1f88
gabrielbrugnoli-netizen <gabriel.brugnoli@escola.pr.gov.br>
2026-06-01 09:37:07 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+const botao = document.getElementById("btnMensagem");
+
+botao.addEventListener("click", () => {
+    alert(
+        "🌾 Bem-vindo ao Projeto Agrinho 2026!\n\nJuntos valorizamos a agricultura, a sustentabilidade e a conexão entre campo e cidade."
+    );
+});
